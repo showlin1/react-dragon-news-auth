39a37f963083d895a0d7fa18db04368db686b00f
unknown <showlinakter77@gmail.com>
2024-05-17 22:16:54 +0600

    create a Register form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,41 @@
+import { Link } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 
 
 const Login = () => {
+    const handleLogin = e => {
+        e.preventDefault();
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        console.log(form.get('password'));
+    }
     return (
         <div>
             <Navbar></Navbar>
-            <h2 className="text-3xl">This is Login</h2>
+            <div>
+                <h2 className="text-3xl text-center my-10">Please Login</h2>
+                <form onSubmit={handleLogin} className="md:w-3/4 lg:w-1/2 mx-auto">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" placeholder="Email" name="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="Password" name="password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Login</button>
+                    </div>
+                </form>
+                <p  className="text-center mt-4">Do not have an account <Link to='/register' className="text-blue-600 font-bold">Register</Link></p>
+            </div>
         </div>
     );
 };
