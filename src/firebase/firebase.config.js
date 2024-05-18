a0b1955984b829a319fee8da5dfcc3042c49fb98
unknown <showlinakter77@gmail.com>
2024-05-18 18:37:01 +0600

    updated firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB8HXqC4872j3zbCN_Go7wgblQbM3Lo-Kg",
-  authDomain: "react-dragon-news-auth-7e11e.firebaseapp.com",
-  projectId: "react-dragon-news-auth-7e11e",
-  storageBucket: "react-dragon-news-auth-7e11e.appspot.com",
-  messagingSenderId: "1041895989431",
-  appId: "1:1041895989431:web:928008f76b97be6aef2d32"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
